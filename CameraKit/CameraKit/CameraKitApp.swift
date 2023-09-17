9c449869c556f4e616053308d38a45c14ebd5115
Abhiraj <abhi266raj@gmail.com>
2023-09-17 19:09:24 +0530

    CameraKit

---
+++ CameraKit/CameraKit/CameraKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraKitApp.swift
+//  CameraKit
+//
+//  Created by Abhiraj on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraKit/CameraKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CameraKit
+//
+//  Created by Abhiraj on 17/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
