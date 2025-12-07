4bd800d02bdc781e0c9e910dce3a7b329aafabcd
Abhiraj <abhi266raj@gmail.com>
2025-12-07 19:12:18 +0530

    Updating module

---
+++ CameraKit/PlatformKit/Sources/PlatformKit.api/CameraProcessor.swift
@@ -0,0 +1,21 @@
+//
+//  CameraProcessor.swift
+//  CameraKit
+//
+//  Created by Abhiraj on 16/10/23.
+//
+
+import Foundation
+import CoreMedia
+import CoreKit
+
+public protocol CameraProccessor {
+    func process(sampleBuffer: CMSampleBuffer) -> CMSampleBuffer
+    func updateSelection(filter: (any FilterModel)?)
+}
+
+extension CameraProccessor {
+    func updateSelection(filter: (any FilterModel)?) {
+    }
+}
+
---
+++ CameraKit/PlatformKit/Sources/PlatformKit.runtime/Processing/EmptyCameraProcessor.swift
@@ -0,0 +1,19 @@
+//
+//  EmptyCameraProcessor.swift
+//  CameraKit
+//
+//  Created by Abhiraj on 14/10/23.
+//
+
+import Foundation
+import CoreMedia
+import PlatformKit_api
+
+
+class EmptyCameraProcessor: CameraProccessor {
+    func process(sampleBuffer: CMSampleBuffer) -> CMSampleBuffer {
+        return sampleBuffer
+    }
+    
+    
+}
