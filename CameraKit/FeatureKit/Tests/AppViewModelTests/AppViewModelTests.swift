4b7083aaaf04f66550418ce1575f30ad78a9d5fa
Abhiraj <abhi266raj@gmail.com>
2025-12-08 16:48:37 +0530

    Fixes

---
+++ CameraKit/FeatureKit/Tests/AppViewModelTests/AppViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  AppViewModelTests.swift
+//  FeatureKit
+//
+//  Created by Abhiraj on 08/12/25.
+//
+
+
+import Testing
+@testable import AppKit
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
