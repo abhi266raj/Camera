52f17cf6c5ac450d86bafb4879cd0d7da6b57eaf
Abhiraj <abhi266raj@gmail.com>
2025-12-10 18:51:59 +0530

    Fixes

---
+++ CameraKit/PlatformKit/Sources/PlatformKit.runtime/Recording/CameraContentRecordingProxyService.swift
@@ -0,0 +1,33 @@
+//
+//  PreviewOnlyService 2.swift
+//  PlatformKit
+//
+//  Created by Abhiraj on 10/12/25.
+//
+
+
+//
+//  PreviewOnlyService.swift
+//  PlatformKit
+//
+//  Created by Abhiraj on 10/12/25.
+//
+
+
+import Foundation
+import Combine
+import AVFoundation
+import CoreKit
+import PlatformKit_api
+
+public class PreviewOnlyService: CameraContentRecordingService {
+    public var cameraModePublisher = CurrentValueSubject<CameraMode, Never>(.preview)
+    public let supportedOutput: CameraAction = []
+    
+    public init() {
+    }
+    
+    public func performAction(action: CameraAction) async throws -> Bool {
+        throw CameraAction.ActionError.invalidInput
+    }
+}
